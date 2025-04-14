db3a99ba22d14f66c5a4e13c7fb0c7953829aa6e
mridulsrivastavaa <115217847+mridulsrivastavaa@users.noreply.github.com>
2025-04-14 22:58:43 +0530

    Removed Unwanted section
    Removed duplicate tag of codeforces

--- student-dashboard/src/App.jsx
+++ student-dashboard/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           element={
             <ProtectedRoute>
               <Home />
-              <Codeforces />
+              {/* <Codeforces /> */}
             </ProtectedRoute>
           }
         />
